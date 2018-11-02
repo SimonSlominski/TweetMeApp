a82bc9bc75ed74829693fe782ef61f5015d68434
Szymon Słomiński <simon.slominski@gmail.com>
2018-11-02 16:38:35 +0100

    data display

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,14 +1,26 @@
 from rest_framework import generics
+from rest_framework import permissions
 from tweets.models import Tweet
 from django.db.models import Q
 from .serializers import TweetModelSerializer
+
+
+class TweetCreateAPIView(generics.CreateAPIView):
+    serializer_class = TweetModelSerializer
+    # Protects from adding a tweet after copying the link and opening a new window in private mode.
+    # Without a previous login, the following message will appear: "detail": "Authentication credentials were not provided."
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
 
     def get_queryset(self):
-        gs = Tweet.objects.all()
+        gs = Tweet.objects.all().order_by("-timestamp")
+        """The other way to sort tweets is to create class Meta: ordering = ['-timestamp'] in models.py + makemigrations/migrate"""
         query = self.request.GET.get("q", None)
         if query is not None:
             gs = gs.filter(
