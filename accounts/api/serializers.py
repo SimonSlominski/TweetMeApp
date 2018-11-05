3a66de2f586d9981b1942674df4abcafb4a86fa2
Szymon Słomiński <simon.slominski@gmail.com>
2018-11-05 14:21:33 +0100

    Following

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
 from rest_framework import serializers
 
 
 User = get_user_model()
 
 class UserDisplaySerializer(serializers.ModelSerializer):
+    follower_count = serializers.SerializerMethodField()
+    url = serializers.SerializerMethodField()
+
     """ Public display serializer """
     class Meta:
         model = User
@@ -12,4 +16,12 @@ class UserDisplaySerializer(serializers.ModelSerializer):
             'username',
             'first_name',
             'last_name',
+            'follower_count',
+            'url',
         ]
+
+    def get_follower_count(self, obj):
+        return 0
+
+    def get_url(self, obj):
+        return reverse_lazy("profiles:detail", kwargs={"username": obj.username})
